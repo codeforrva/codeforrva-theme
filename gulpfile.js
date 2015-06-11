6f7e1ba993c60267fc9a7fc13a168fe8e52992ea
adamhake <adamhake@gmail.com>
2015-06-10 20:00:34 -0400

    initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp         = require('gulp'),
+    browserSync  = browserSync || require('browser-sync').create(),
+    plumber			 = require('gulp-plumber'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCss    = require('gulp-minify-css');
+
+gulp.task('sass', function () {
+    gulp.src('src/scss/styles.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(minifyCss())
+        .pipe(gulp.dest('build/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("src/scss/**/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
