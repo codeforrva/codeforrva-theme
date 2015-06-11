8178c3f05a9ecdedf1b6269405ae968e4d2f1f55
Tony Gambone <tonygambone@gmail.com>
2015-06-11 18:39:40 -0400

    Better formatting for the meetup box.

--- plugins/meetup-widgets/meetup-single.php
+++ plugins/meetup-widgets/meetup-single.php
@@ -1,33 +1,24 @@
 <?php 
 global $event; 
 if ( isset( $event->time ) ) {
-	$date = date( 'F d, Y @ g:i a', intval( $event->time/1000 + $event->utc_offset/1000 ) );
+	$date = date( 'l, F j @ g:i a', intval( $event->time/1000 + $event->utc_offset/1000 ) );
 } else {
 	$date = apply_filters( 'vsm_no_date_text', '' );
 }
 ?>
 
-<h3 class="event-title"><a href="<?php echo esc_url($event->event_url); ?>"><?php echo strip_tags($event->name); ?></a></h3>
+<h3 class="next-meetup__title">Our Next Meetup</h3>
 <?php if ( ! empty( $date ) ): ?>
-<p class="event-date"><?php echo $date; ?></p>
+<p class="next-meetup__date"><?php echo $date; ?></p>
 <?php endif; ?>
-<p class="event-summary"><?php echo wp_trim_words( strip_tags( $event->description ), 20 ); ?></p>
-<p class="event-rsvp"><span class="rsvp-count"><?php echo absint( $event->yes_rsvp_count ) .' '. _n( 'attendee', 'attendees', $event->yes_rsvp_count ); ?></span>
+<p class="next-meetup__location"><?php echo (isset($event->venue) ? $event->venue->name : "INM United"); ?></p>
+<p class="next-meetup__attendees"><?php echo absint( $event->yes_rsvp_count ) .' '. _n( 'attendee', 'attendees', $event->yes_rsvp_count ); ?></p>
 
+<a class="next-meetup__rsvp"
 <?php
 if ( !empty($options['vs_meetup_key']) && !empty($options['vs_meetup_secret']) && class_exists('OAuth')) {
-	echo "<span class='rsvp-add'><a href='#' onclick='javascript:window.open(\"{$event->callback_url}&event=$id\",\"authenticate\",\"width=400,height=600\");'>RSVP?</a></span>";
+	echo "href='#' onclick='javascript:window.open(\"{$event->callback_url}&event=$id\",\"authenticate\",\"width=400,height=600\");'";
 } else {
-	echo '<span class="rsvp-add"><a href="'.$event->event_url.'">RSVP?</a></span>';
-} ?></p>
-
-<?php
-if ( isset( $event->venue ) ) {
-	$venue = $event->venue->name.' '.$event->venue->address_1 . ', ' . $event->venue->city . ', ' . $event->venue->state;
-	echo "<p class='event-location'>Location: <a href='http://maps.google.com/maps?q=$venue+%28".$event->venue->name."%29&z=17'>$venue</a></p>";
-} else {
-	$venue = apply_filters( 'vsm_no_location_text', "Location: TBA" );
-	if ( ! empty( $venue ) ){
-		echo "<p class='event-location'>$venue</p>";
-	}
-}
+	echo 'href="'.$event->event_url.'"';
+} ?>
+>RSVP Now!</a>
